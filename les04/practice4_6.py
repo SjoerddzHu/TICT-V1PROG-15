48cdaeb8f40c06132e4b09c765cdbd07d1a1eee9
sjoerddzHu <sjoerd.dezaaijer@student.hu.nl>
2017-09-15 11:37:56 +0200

    mijn huiswerk

--- les04/practice4_6.py
+++ les04/practice4_6.py
@@ -1,6 +1,11 @@
 
 lijst = ['a', 'b', 'c']
 print(lijst)
-def wijzig():
-    lijst.clear()
-print(lijst)
+def wijzig(lijst):
+    mijnlijst = []
+    mijnlijst.append('d')
+    mijnlijst.append('e')
+    mijnlijst.append('f')
+    return (mijnlijst)
+print(wijzig(lijst))
+
